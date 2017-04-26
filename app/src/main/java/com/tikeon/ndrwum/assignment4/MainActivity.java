29ea5f50bbc2990a8a346591d45761b055d850c1
ndrwum <ndrwum@yahoo.com>
2017-04-26 15:44:58 -0400

    added maps
    scroll view

--- app/src/main/java/com/tikeon/ndrwum/assignment4/MainActivity.java
+++ app/src/main/java/com/tikeon/ndrwum/assignment4/MainActivity.java
@@ -1,17 +1,39 @@
 package com.tikeon.ndrwum.assignment4;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,29 +45,36 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, MediaPlayer.OnPreparedListener {
 
+    private static MediaPlayer mPlayer;
+    protected GoogleApiClient mGoogleApiClient;
     EditText cityName;
     TextView resultTextView;
+    private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+    Location loc;
 
     public void findWeather(View view) {
-
-        Log.i("cityName", cityName.getText().toString());
 
         InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
 
         try {
             String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8");
+            String encodedLatitude = URLEncoder.encode(String.valueOf(loc.getLatitude()), "UTF-8");
+            String encodedLongitude = URLEncoder.encode(String.valueOf(loc.getLongitude()), "UTF-8");
 
             DownloadTask task = new DownloadTask();
-            task.execute("http://api.openweathermap.org/data/2.5/forecast?q=" + encodedCityName+"&"+"appid=df25a7ba0fc76dce7171effa6c0cedd3");
-
+            if (encodedCityName!="") {
+                task.execute("http://api.openweathermap.org/data/2.5/forecast?q=" + encodedCityName + "&" + "appid=df25a7ba0fc76dce7171effa6c0cedd3");
+            } else if (loc!=null){
+                task.execute("http://api.openweathermap.org/data/2.5/forecast?lat=" + encodedLatitude + "&" + "lon=" + encodedLongitude + "appid=df25a7ba0fc76dce7171effa6c0cedd3");
+            }
 
         } catch (UnsupportedEncodingException e) {
 
@@ -56,7 +85,6 @@ public class MainActivity extends Activity {
         }
 
 
-
     }
 
     @Override
@@ -64,15 +92,149 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        if (mGoogleApiClient == null) {
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         cityName = (EditText) findViewById(R.id.cityName);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
+        playMusic();
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        loc=location;
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mMap.animateCamera(cameraUpdate);
+    }
+
+    public void playMusic() {
+        String sss = "https://www.partnersinrhyme.com/pir/libs/media/Analog_Boys_2.wav";
+        if (mPlayer == null || !mPlayer.isPlaying()) {
+            mPlayer = new MediaPlayer();
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mPlayer.setDataSource(sss);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mPlayer.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mPlayer != null) {
+            mPlayer.stop();
+        }
+        mGoogleApiClient.disconnect();
     }
 
 
-    public class DownloadTask extends AsyncTask<String, Void, String> {
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mPlayer != null) {
+            mPlayer.start();
+        }
+        if (mGoogleApiClient.isConnected()) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mPlayer != null) {
+            mPlayer.pause();
+        }
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        if (mLocationRequest == null) {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.setInterval(2000);
+            mLocationRequest.setFastestInterval(1000);
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
@@ -120,10 +282,12 @@ public class MainActivity extends Activity {
             return null;
         }
 
+        /*Parsing JSON weather info from API*/
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
+            String weather = null;
+            String description = null;
             try {
 
                 String message = "";
@@ -134,33 +298,46 @@ public class MainActivity extends Activity {
 
                 JSONArray arr = new JSONArray(weatherInfo);
 
-                for (int i = 0; i<arr.length(); i++){
+                for (int i = 0; i < arr.length(); i++) {
 
                     JSONObject tmp = arr.getJSONObject(i);
 
-                    String weather = tmp.getString("weather");
+                    weather = tmp.getString("weather");
 
                     JSONArray mainWeather = new JSONArray(weather);
 
-                    for (int j = 0; j<mainWeather.length(); j++){
+                    for (int j = 0; j < mainWeather.length(); j++) {
 
                         tmp = mainWeather.getJSONObject(j);
 
-                        weather = tmp.getString("main");
-                        String description = "";
-                        description = tmp.getString("description");
-                        Log.i("Weather ", weather);
-                        Log.i("Description ", description);
-                        if (weather != "" && description != "") {
+                        weather += tmp.getString("main");
 
-                            message += weather + ": " + description + "\r\n";
-
-                        }
+                        description += tmp.getString("description");
 
                     }
 
+
                 }
 
+                /*Weather Conditionals*/
+                if (weather != "" && description != "") {
+                    assert description != null;
+                    if (description.toLowerCase().contains("rain")) {
+                        message += "There's going to be rain today." + "\r\n";
+                    }
+                }
+                if (weather != "" && description != "") {
+                    if (description != null && description.toLowerCase().contains("rain")) {
+                        message += "There's not going to be rain today." + "\r\n";
+                    }
+                }
+                if (weather != "" && description != "") {
+                    if (description != null && description.toLowerCase().contains("snow")) {
+                        message += "There's going to be snow today." + "\r\n";
+                    }
+                }
+
+                /*No Weather Data Retrieved*/
                 if (message != "") {
 
                     resultTextView.setText(message);
@@ -179,30 +356,7 @@ public class MainActivity extends Activity {
             }
 
 
-
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
 
